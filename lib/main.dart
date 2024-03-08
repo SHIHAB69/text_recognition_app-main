2d8f02bfafd0c762cd9108be3ecfccb790ecb185
SHIHAB69 <southcybersoldier@gmail.com>
2024-03-08 21:09:01 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,119 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Text Extraction',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  File? _imageFile;
+  String? _extractedText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Text Extraction'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _imageFile == null
+                ? const Text('No image selected.')
+                : Image.file(_imageFile!),
+            const SizedBox(height: 20),
+            _extractedText == null
+                ? const Text('No text extracted.')
+                : Text('Extracted Text: $_extractedText'),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _pickImage,
+              child: const Text('Select Image'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _extractAndSaveText,
+              child: const Text('Extract and Save Text'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> _pickImage() async {
+    final imagePicker = ImagePicker();
+    final pickedFile = await imagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _imageFile = File(pickedFile.path);
+        _extractedText = null;
+      } else {
+         print('No image selected.');
+      }
+    });
+  }
+
+  Future<void> _extractAndSaveText() async {
+    if (_imageFile == null) {
+      return;
+    }
+
+    final inputImage = InputImage.fromFile(_imageFile!);
+    final textRecognizer = GoogleMlKit.vision.textRecognizer();
+    final RecognizedText recognisedText = await textRecognizer.processImage(inputImage);
+
+    setState(() {
+      _extractedText = recognisedText.text;
+    });
+
+    await _saveExtractedTextToFirestore(_extractedText!);
+  }
+
+  //save the extracted text to firestore
+
+  Future<void> _saveExtractedTextToFirestore(String text) async {
+     {
+      await Firebase.initializeApp();
+      final firestore = FirebaseFirestore.instance;
+      await firestore.collection('extracted_text').add({
+        'text': text,
+      });
+      print('Text saved to Firestore.');
+
+    }
+  }
+}
+
+//Sihab | SWE(232) | Diu
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,20 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import cloud_firestore
+import device_info_plus
+import file_selector_macos
+import firebase_core
+import video_player_avfoundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
+}
